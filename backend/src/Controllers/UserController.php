6f2f78c9b00f0090e4b1c2168cca9ae0a4cdc944
Jared <TheWolfBadger@gmail.com>
2024-06-13 22:59:40 -0400

    Backend login and register is being worked on

--- backend/src/Controllers/UserController.php
+++ backend/src/Controllers/UserController.php
@@ -17,11 +17,43 @@ class UserController extends Routes {
     $email = $body['email'];
     $password = $body['password'];
     $username = $body['username'];
+    $pdo = $this->dbService->getConnection();
+    $pdo->beginTransaction();
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM users WHERE `email` = :email OR `username` = :username");
+    $stmt->execute(['email' => $email, 'username' => $username]);
+    $userOrEmailExists = $stmt->fetchColumn();
+    if ($userOrEmailExists == 0) {
+      $stmt = $pdo->prepare("INSERT INTO `users` (`email`, `username`, `password`) VALUES (:email, :username, :password)");
+      $stmt->execute(['email' => $email, 'username' => $username, 'password' => $password]);
+      $pdo->commit();
+    } else {
+      // TODO Error, this username or email already exists...
+    }
   }
   protected function login(Request $request, Response $response, $args) {
     $body = $request->getParsedBody();
     $email = $body['email'];
     $password = $body['password'];
     $username = $body['username'];
+    $pdo = $this->dbService->getConnection();
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM users WHERE `email` = :email OR `username` = :username");
+    $stmt->execute(['email' => $email, 'username' => $username]);
+    $userOrEmailExists = $stmt->fetchColumn();
+    if ($userOrEmailExists) {
+      // Valid user, is password correct?
+      $stmt = $pdo->prepare("SELECT `password` FROM `users` WHERE `email` = :email OR `username` = :username");
+      $stmt->execute(['email' => $email, 'username' => $username]);
+      $passwordRet = $stmt->fetchColumn();
+      if (password_verify($password, $passwordRet)) {
+        // Valid password
+        // TODO
+      } else {
+        // Not a valid password
+        // TODO
+      }
+    } else {
+      // Not a valid user, inform them...
+      // TODO
+    }
   }
 }
